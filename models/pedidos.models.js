8375c5bd64c068a64ad4070d17e3432a2b188a40
Serena <rotondaserena3@gmail.com>
2025-09-25 22:49:57 -0300

    models terminado

---
+++ models/pedidos.models.js
@@ -0,0 +1,23 @@
+export function Pedido({
+  id,
+  nombre,
+  direccion,
+  telefono,
+  precio,
+  envio,
+  propina,
+  preciototal,
+  forma_de_pago,
+}) {
+  return {
+    id,
+    nombre,
+    direccion,
+    telefono,
+    precio,
+    envio,
+    propina,
+    preciototal,
+    forma_de_pago,
+  };
+}
